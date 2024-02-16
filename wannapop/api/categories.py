6d3455e25fd6e3338aecd1d2f561256606a341f6
«alexmpDev» <alexmpDev@gmail.com>
2024-02-16 15:28:16 +0100

    resolve conflicts

--- wannapop/api/categories.py
+++ wannapop/api/categories.py
@@ -1,22 +1,3 @@
-<<<<<<< HEAD
-from . import api_bp
-from ..models import Category, Product
-from ..helper_json import json_response
-from flask import current_app
-
-# List
-@api_bp.route('/category', methods=['GET'])
-def get_category():
-    categories = Category.get_all()
-    data = Category.to_dict_collection(categories)
-    return json_response(data)
-
-# Items list
-@api_bp.route('/categories/<int:id>/product', methods=['GET'])
-def get_category_product(id):
-    product = Product.get_all_filtered_by(cateogry_id=id)
-    data = Product.to_dict_collection(product)
-=======
 from . import api_bp
 from .errors import not_found
 from .. import db_manager as db
@@ -28,5 +9,4 @@ from flask import current_app
 def get_api_categories():
     categories = Category.get_all()
     data = Category.to_dict_collection(categories)
->>>>>>> 34fa2dd9d57cdc8b99fc46b8ad56e54c7dc0ede4
     return json_response(data)
--- wannapop/api/errors.py
+++ wannapop/api/errors.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 from . import api_bp
 from ..helper_json import json_error_response
 from werkzeug.exceptions import HTTPException
@@ -16,23 +15,4 @@ def not_found(message):
 # For example: abort(401)
 @api_bp.errorhandler(HTTPException)
 def handle_exception(e):
-=======
-from . import api_bp
-from ..helper_json import json_error_response
-from werkzeug.exceptions import HTTPException
-
-def bad_request(message):
-    return json_error_response(400, message)
-
-def forbidden_access(message):
-    return json_error_response(403, message)
-
-def not_found(message):
-    return json_error_response(404, message)
-
-# Catch all HTTP exceptions
-# For example: abort(401)
-@api_bp.errorhandler(HTTPException)
-def handle_exception(e):
->>>>>>> 34fa2dd9d57cdc8b99fc46b8ad56e54c7dc0ede4
     return json_error_response(e.code)
